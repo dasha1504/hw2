2dac7dab1379cc67541e8e12a8e741109ac11366
dasha <dasha15042009@yandex.ru>
2022-08-26 12:12:18 +0300

    Ready

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+//First ex.
+Console.WriteLine("Input number ");
+int a = int.Parse(Console.ReadLine());
+int b = (a/10)%10;
+Console.WriteLine(b);
+
+//Second ex
+/* Console.WriteLine("Input number ");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+if(a/100 < 0.99)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+else
+{
+    while(a/1000 >= 0.1)
+    a = a/10;
+    b = a%10;
+Console.WriteLine(b);
+} */
+
+
+//Third ex
+/* Console.WriteLine("Input number ");
+int a = int.Parse(Console.ReadLine());
+if(a > 0 & a < 6)
+{
+    Console.WriteLine("No");
+}
+else if(a == 6 || a == 7)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("Input number from 1 to 7");
+} */
